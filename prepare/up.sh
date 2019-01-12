6015aac70193abac71f5fdcd08940cd2960e4810
ƬHE ЯAW ☣ <raw@dopehosting.net>
2019-01-12 20:49:24 +0100

    Create up.sh

---
+++ prepare/up.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mysqlup() {
+    mkdir -p /root/workdir/
+    docker pull theraw/rocky:mysql
+    cd /root/workdir/
+    curl -s https://raw.githubusercontent.com/DopeProjects/openstack-rocky-docker/master/containers/docker-compose-mysql.yml /root/workdir/docker-compose.yml
+    cd /root/workdir/; docker-compose up -d
+}
