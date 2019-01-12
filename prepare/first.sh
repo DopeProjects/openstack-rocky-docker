00bff364ae5e8f3f152ec7d50d0e6b69d5fbb6bc
ƬHE ЯAW ☣ <raw@dopehosting.net>
2019-01-12 15:22:07 +0100

    Create first.sh

---
+++ prepare/first.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+check() {
+          if ! [ -x "$(command -v docker)" ]; then
+            echo 'Please install Docker to continue..';
+            exit 1
+          fi
+
+          if [ "$(whoami)" != "raw" ]; then
+            echo 'Please login as root because this script has not been writed with "sudo"';
+            exit 1
+          fi
+}
+
+pull() {
+          docker pull ubuntu:18.10
+}
+
