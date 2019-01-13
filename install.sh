890d8a4dca8783c98426a7ac474e561de1c6bb89
ƬHE ЯAW ☣ <raw@dopehosting.net>
2019-01-13 23:02:54 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ mkdir -p /opt/rocky
 mkdir -p /opt/rocky/prepare
 curl -s https://raw.githubusercontent.com/DopeProjects/openstack-rocky-docker/master/bin/rocky > /bin/rocky
 curl -s https://raw.githubusercontent.com/DopeProjects/openstack-rocky-docker/master/prepare/first.sh > /opt/rocky/prepare/first
+curl -s https://raw.githubusercontent.com/DopeProjects/openstack-rocky-docker/master/prepare/up.sh > /opt/rocky/prepare/up
 
 
 chmod +x /bin/rocky
