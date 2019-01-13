698dbc6538cb30c9e05c82e6a0324ed781be6107
ƬHE ЯAW ☣ <raw@dopehosting.net>
2019-01-13 23:15:23 +0100

    Update first.sh

--- prepare/first.sh
+++ prepare/first.sh
@@ -14,7 +14,9 @@ check() {
 
 pull() {
           clear
+          docker network create --subnet=172.69.0.0/16 rocky
           docker pull ubuntu:18.10
+          docker pull theraw/rocky:mysql
           clear
           docker images
 }
