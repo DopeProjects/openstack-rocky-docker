25abb3d54440ed9d3d38da02959a8df331a5dfc1
ƬHE ЯAW ☣ <raw@dopehosting.net>
2019-01-13 23:03:46 +0100

    Update up.sh

--- prepare/up.sh
+++ prepare/up.sh
@@ -6,4 +6,7 @@ mysqlup() {
     cd /root/workdir/
     curl -s https://raw.githubusercontent.com/DopeProjects/openstack-rocky-docker/master/containers/docker-compose-mysql.yml /root/workdir/docker-compose.yml
     cd /root/workdir/; docker-compose up -d
+    cd ~/; rm -Rf /root/workdir/
+    clear
+    docker ps
 }
